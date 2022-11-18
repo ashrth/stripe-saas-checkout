c186478fe35541b89ac587af87c0693c313975bb
ashrth <aashrrita@gmail.com>
2022-11-18 13:52:23 +0530

    prices added

--- src/ProductDisplay.js
+++ src/ProductDisplay.js
@@ -2,19 +2,17 @@ import {Link, Routes, Route } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 const ProductDisplay = () => (
-    <section className="head">
+<>
+<section className="head">
       <div className="product">
-        <img
-          src="https://i.imgur.com/EHyR2nP.png"
-          alt="The cover of Stubborn Attachments"
-        />
+        
         <div className="description">
-          <h3>Stubborn Attachments</h3>
-          <h5>$20.00</h5>
+          <h3>Growth Package</h3>
+          <h5>$2,450.00 USD / month</h5>
         </div>
       </div>
       <form
-        action="https://stripetestcheckout.herokuapp.com/api/v1/main/create-checkout-session"
+        action="https://stripetestcheckout.herokuapp.com/api/v1/main/create-checkout-session-scale-growth"
         method="POST"
       >
         <div className="tac">
@@ -25,6 +23,51 @@ const ProductDisplay = () => (
       
   
       </section>
+
+      <section className="head">
+      <div className="product">
+        
+        <div className="description">
+          <h3>Scale Package</h3>
+          <h5>$3,150.00 USD / month</h5>
+        </div>
+      </div>
+      <form
+        action="https://stripetestcheckout.herokuapp.com/api/v1/main/create-checkout-session-scale"
+        method="POST"
+      >
+        <div className="tac">
+        <Link to="/terms-and-conditions">Terms and Conditions</Link>
+      </div>
+        <button  type="submit">Checkout</button>
+      </form>
+      
+  
+      </section>
+
+      <section className="head">
+      <div className="product">
+        
+        <div className="description">
+          <h3>Growth Package + Scale Package</h3>
+          <h5>$5,600.00 USD / month</h5>
+        </div>
+      </div>
+      <form
+        action="https://stripetestcheckout.herokuapp.com/api/v1/main/create-checkout-session-both"
+        method="POST"
+      >
+        <div className="tac">
+        <Link to="/terms-and-conditions">Terms and Conditions</Link>
+      </div>
+        <button  type="submit">Checkout</button>
+      </form>
+      
+  
+      </section>
+</>
+    
+      
   );
   const Message = ({ message }) => (
     <section>
